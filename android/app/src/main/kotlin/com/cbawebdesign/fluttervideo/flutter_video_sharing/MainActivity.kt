3a856f68fb739d387bd0be98a0dc4ca63eeb9479
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2020-07-12 04:10:25 -0700

    commit

---
+++ android/app/src/main/kotlin/com/cbawebdesign/fluttervideo/flutter_video_sharing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cbawebdesign.fluttervideo.flutter_video_sharing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/apis/firebase_provider.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_video_sharing/video_info.dart';
+
+class FirebaseProvider {
+  static saveVideo(VideoInfo video) async {
+    await Firestore.instance.collection('videos').document().setData({
+        "videoUrl": video.videoUrl,
+        "thumbUrl": video.thumbUrl,
+        "coverUrl": video.coverUrl,
+        "aspectRatio": video.aspectRatio,
+      });
+  }
+
+  static listenToVideos(callback) async {
+    Firestore.instance.collection('videos').snapshots().listen((qs) {
+      final videos = mapQueryToVideoInfo(qs);
+      callback(videos);
+    });
+  }
+
+  static mapQueryToVideoInfo(QuerySnapshot qs) {
+    return qs.documents.map((DocumentSnapshot ds) {
+      return VideoInfo(
+        videoUrl: ds.data["videoUrl"],
+        thumbUrl: ds.data["thumbUrl"],
+        coverUrl: ds.data["coverUrl"],
+        aspectRatio: ds.data["aspectRatio"],
+      );
+    }).toList();
+  }
+}
